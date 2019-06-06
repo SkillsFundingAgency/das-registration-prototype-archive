6f245890ce8d0e35a6f513bf2aa4282493915230
Michael Chambers <michaelchambersjnr@gmail.com>
2019-06-06 09:04:07 +0100

    Updates made to prototype start page

--- app/routes.js
+++ app/routes.js
@@ -3,5 +3,11 @@ const router = express.Router()
 
 require('./routes/1-0/routes.js')(router);
 require('./routes/2-0/routes.js')(router);
+require('./routes/3-0/routes.js')(router);
+
+// Employer started
+router.get('/directEmployer', function (req, res) {
+    res.redirect(`/1-0/registration/`)
+    })
 
 module.exports = router
